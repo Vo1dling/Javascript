6dba1ee373d3498a132be8c29c7dcade0d5154d9
Vo1dling <attlanticazz@hotmail.com>
2021-11-07 14:53:14 +0200

    Java

---
+++ 1.1 primitive data type.js
@@ -0,0 +1,14 @@
+let string = 'string'
+let number = 11
+let object = {}
+let boolean = false
+let nullValue = null
+let undefinedV = undefined
+let symbol = Symbol('dsa')
+console.log(string)
+console.log(number)
+console.log(boolean)
+console.log(object)
+console.log(nullValue)
+console.log(undefinedV)
+console.log(symbol)
